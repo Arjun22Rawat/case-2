d30bb946d5ac65db22d922dcdaae1c4af53f526d
Rawat <arjun.a.rawat@capgemini.com>
2023-10-16 15:14:53 +0530

    first commit

---
+++ DAC/src/app/profile/profile.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CredentialService } from '../credential.service';
+import { Router } from '@angular/router';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent 
+{
+
+  details:any;
+  constructor(private service:CredentialService,private router:Router, private app:AppComponent){}
+
+  ngOnInit()
+  {
+    this.service.getCustomerDetails().subscribe((data: any)=> {
+      this.details=JSON.parse(data);
+    });
+  }
+
+  logout()
+  {
+    this.service.userObj= {};
+    this.app.isLogin = false;
+    this.router.navigate(["/home"]);
+  }
+}
